d88c71e1d87a217190a5ab29ebec07ad4e31739e
kieroc44 <kierolleskamel@gmail.com>
2025-06-26 14:24:52 +0300

    Add project files.

---
+++ AssignmentOneC#/Program.cs
@@ -0,0 +1,86 @@
+﻿namespace AssignmentOne
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Task 
+            /*
+             * Ask user for salary amount and print:    
+             * "No tax" if < 5000 , and the salary stays the same.
+             * "10% tax" if between 5000–9999 ,  subtract 10% tax.
+             * "20% tax" if ≥ 10000 ,  subtract 20% tax.
+             * Expected output : 
+             * ==> 10% tax applied  
+             * ==> Final salary after tax 
+             */
+            #endregion
+
+            #region 1-	Write a program that allows the user to enter a number then print it.
+
+            #endregion
+
+            #region 2-	Write C# program that converts a string to an integer, but the string contains non-numeric characters. And mention what will happen 
+
+            #endregion
+
+            #region 3 -    Write C# program that Perform a simple arithmetic operation with floating-point numbers And mention what will happen
+
+
+            #endregion
+
+            #region 4-	Write C# program that Extract a substring from a given string.
+
+            #endregion
+
+            #region 5-     Write C# program that Assigning one value type variable to another and modifying the value of one variable and mention what will happen
+
+            #endregion
+
+            #region 6-	 Write C# program that Assigning one reference type variable to another and modifying the object through one variable and mention what will happen
+
+            #endregion
+
+            #region 7-	Write C# program that take two string variables and print them as one variable 
+
+            #endregion
+
+            #region 8-	Write a program that calculates the simple interest given the principal amount, rate of interest, and time. The formula for simple interest is Interest = (principal * rate * time) / 100.
+
+
+            #endregion
+
+            #region 9-	Write a program that calculates the Body Mass Index (BMI) given a person's weight in kilograms and height in meters. The formula for BMI is BMI = (Weight) / (Height * Height)
+
+
+            #endregion
+
+            #region 10-	Write a program that takes the date from the user and displays it in various formats using string interpolation.
+            //Ex:
+            //Today’s date : 20 , 11 , 2001
+            //Today's date : 20 / 11 / 2001
+            //Today's date : 20 – 11 – 2001
+
+
+            #endregion
+
+            #region 11-	 What is the output of the following C# code?
+            /*  
+             DateTime date = new DateTime(2024, 6, 14);
+             Console.WriteLine($"The event is on {date:MM/dd/yyyy}");*/
+            #endregion
+
+            #region 12-	Which of the following statements is correct about the C#.NET code snippet given below?
+            /*
+             * int d; 
+             * d = Convert.ToInt32( !(30 < 20) );
+             */
+            #endregion
+
+            #region 13-	Which of the following is the correct output for the C# code given below?
+            /*Console.WriteLine(13 / 2 + " " + 13 % 2); */
+
+            #endregion
+        }
+    }
+}
