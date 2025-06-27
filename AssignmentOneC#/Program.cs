c7f464473633de3a4f75acdf3700ce0b538c0374
kieroc44 <kierolleskamel@gmail.com>
2025-06-27 21:01:46 +0300

    Q11

--- AssignmentOneC#/Program.cs
+++ AssignmentOneC#/Program.cs
@@ -184,6 +184,11 @@
             /*  
              DateTime date = new DateTime(2024, 6, 14);
              Console.WriteLine($"The event is on {date:MM/dd/yyyy}");*/
+
+            //The event is on 06/14/2024
+
+
+
             #endregion
 
             #region 12-	Which of the following statements is correct about the C#.NET code snippet given below?
